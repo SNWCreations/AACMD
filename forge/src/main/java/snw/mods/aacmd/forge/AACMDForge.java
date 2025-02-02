e069422b69e0f3563c88b9f8669b8dbe223090b3
ZX夏夜之风 <snwcreations@qq.com>
2025-02-02 11:42:06 +0800

    refactor: remove unnecessary dependency on Architectury API and Fabric API

--- forge/src/main/java/snw/mods/aacmd/forge/AACMDForge.java
+++ forge/src/main/java/snw/mods/aacmd/forge/AACMDForge.java
@@ -1,16 +1,12 @@
 package snw.mods.aacmd.forge;
 
 import snw.mods.aacmd.AACMD;
-import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AACMD.MOD_ID)
 public final class AACMDForge {
     public AACMDForge(FMLJavaModLoadingContext loadingContext) {
-        // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(AACMD.MOD_ID, loadingContext.getModEventBus());
-
         // Run our common setup.
         AACMD.init();
     }
